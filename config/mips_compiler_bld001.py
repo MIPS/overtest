f93c3ed57e71325ae8ebea6c7b6aeec461ea3be4
Matthew Fortune <matthew.fortune@imgtec.com>
2018-06-05 12:22:12 -0700

    Add config for MIPS Compiler BLD001

---
+++ config/mips_compiler_bld001.py
@@ -0,0 +1,15 @@
+"""
+Module for mips-compiler-bld001 config
+"""
+import config.centos5
+
+class Config(config.centos5.Config):
+  """
+  Class for mips-compiler-bld001 config
+  """
+  fqdn = "mips-compiler-bld001"
+  localdir = "/scratch/overtest/root/"
+  logdir = "/scratch/overtest/root/"
+  shareddir = "/scratch/overtest/root/"
+  python = "/mips/tools/sweng/freeware/python/2.7.13/Linux/bin/python"
+
