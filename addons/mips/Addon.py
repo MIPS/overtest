e5db69a48dc722cf5191ce8451909eac4f819e64
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-08-22 21:47:52 -0700

    Fix mti config filtering

--- addons/mips/Addon.py
+++ addons/mips/Addon.py
@@ -143,7 +143,9 @@ class Addon:
           continue
         if vendor[:3] == "mti" and "img" in self.runlist:
           continue
-        if vendor[3] == "mti" and "mti-only" in self.runlist:
+        if vendor == "mti" and "mti-only" in self.runlist:
+          continue
+        if vendor == "mti-only" and "mti-only" not in self.runlist:
           continue
         for os_part in ["elf", "linux-gnu"]:
           if not os_part in self.runlist:
