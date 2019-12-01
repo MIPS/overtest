71f4131e1edaf68bd97597ddfcde830baeb88b6a
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-12-01 14:18:08 -0800

    Change qemu repo name to gnutools-qemu

--- action/A117821.py
+++ action/A117821.py
@@ -13,4 +13,4 @@ class A117821(Action):
   # scripts/archive-source.sh within the qemu source tree which needs
   # a deep check-out to work correctly.
   def run(self):
-    return self.gitFetch("qemu.git", deep=True)
+    return self.gitFetch("gnutools-qemu.git", deep=True)
