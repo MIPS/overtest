a577bd5d195e9d8101595584507be4649f88b5ab
Faraz Shahbazker <fshahbazker@wavecomp.com>
2020-06-16 05:53:33 -0700

    Disable coloured output for UBSAN to fix gcc regression mismatches
    Previous commit only fixes G++ test environment.

--- action/A117823.py
+++ action/A117823.py
@@ -122,6 +122,8 @@ class A117823(Action, GCC4RegressionParser):
         env['DEJAGNU_SIM'] = qemu_exec
         env['DEJAGNU_SIM_OPTIONS'] = "-r %s -cpu %s" % (kernel_version, cpu)
         env['DEJAGNU_SIM_GCC'] = gcc_exec
+        env['ASAN_OPTIONS'] = "color=never"
+        env['UBSAN_OPTIONS'] = "color=never"
       else:
         board = "generic-sim"
         if abi == "32" or abi == "p32":
